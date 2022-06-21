f9eecec3900f4128a0bd1a0095f4502123ada4d2
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2022-06-21 21:25:40 +0700

    First commit

---
+++ subscriber.js
@@ -0,0 +1,36 @@
+const mqtt = require('mqtt')
+
+const host = 'mqtt://localhost:8888'
+
+const options = {
+    keepalive: 60
+}
+
+console.log('Connecting to mqtt broker ....')
+
+const client = mqtt.connect(host, options)
+
+client.on('connect', function () {
+    // Subscribe any topic
+    console.log("Mqtt broker connected");
+    client.subscribe('ades/hello', function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+});
+
+// Receive Message and print on terminal
+client.on('message', function (topic, message) {
+    // message is Buffer
+    console.log('received ' + topic + '/' + message.toString());
+});
+
+client.on('error', (err) => {
+    console.log('Connection error: ', err)
+    client.end()
+})
+  
+client.on('reconnect', () => {
+    console.log('Reconnecting...')
+})
